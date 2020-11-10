73c6a731ff903840e84c3e5a2e65183bffe010e4
Steve Gordon <sgordon@hotmail.co.uk>
2020-11-10 16:17:31 +0000

    Add initial benchmark project to sln.
    Supports writing benchmarks to understand current performance and impact of changes.
    
    Includes an initial transport benchmark for a successful request.

---
+++ benchmarks/Elastic.Transport.Benchmarks/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Elastic.Transport.Benchmarks
+{
+	internal class Program
+    {
+	    private static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+	}
+}
---
+++ benchmarks/Elastic.Transport.Benchmarks/TransportBenchmarks.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using Elastic.Transport.VirtualizedCluster;
+
+namespace Elastic.Transport.Benchmarks
+{
+	[MemoryDiagnoser]
+	public class TransportBenchmarks
+	{
+		private static readonly VirtualizedCluster.Components.VirtualizedCluster ClusterWithSuccess = Virtual.Elasticsearch
+			.Bootstrap(1)
+			.ClientCalls(c => c.SucceedAlways())
+			.StaticConnectionPool()
+			.Settings(s => s.DisablePing());
+		
+		[Benchmark]
+		public void TransportSuccessfulRequestBenchmark() => ClusterWithSuccess.Transport.Get<R>("/");
+
+		private class R : ITransportResponse
+		{
+			public IApiCallDetails ApiCall { get; set; }
+		}
+	}
+}
