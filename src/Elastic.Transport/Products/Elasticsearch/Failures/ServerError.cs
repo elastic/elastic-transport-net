b94f715fca56d94f2a8048f8a42dd48b64762f48
Martijn Laarman <Mpdreamz@gmail.com>
2020-11-18 20:24:16 +0100

    ServerError constructor needs to be public

--- src/Elastic.Transport/Products/Elasticsearch/Failures/ServerError.cs
+++ src/Elastic.Transport/Products/Elasticsearch/Failures/ServerError.cs
@@ -15,7 +15,7 @@ namespace Elastic.Transport.Products.Elasticsearch.Failures
 	[DataContract]
 	public class ServerError
 	{
-		internal ServerError() { }
+		public ServerError() { }
 
 		/// <inheritdoc cref="ServerError"/>
 		public ServerError(Error error, int? statusCode)
