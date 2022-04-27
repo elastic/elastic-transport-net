a113492822a43c6bdc7dc1e73203755d6adae23c
Steve Gordon <sgordon@hotmail.co.uk>
2022-04-27 15:31:36 +0100

    Simplify the regex used to match the version

--- src/Elastic.Transport/Requests/MetaData/ReflectionVersionInfo.cs
+++ src/Elastic.Transport/Requests/MetaData/ReflectionVersionInfo.cs
@@ -11,7 +11,7 @@ namespace Elastic.Transport
 {
 	internal sealed class ReflectionVersionInfo : VersionInfo
 	{
-		private static readonly Regex VersionRegex = new(@"(\d+\.)(\d+\.)(\d)\-?");
+		private static readonly Regex VersionRegex = new(@"^\d+\.\d+\.\d\-?");
 
 		public static readonly ReflectionVersionInfo Empty = new() { Version = new Version(0, 0, 0), IsPrerelease = false };
 
@@ -37,7 +37,7 @@ namespace Elastic.Transport
 		{
 			try
 			{
-				var productVersion = FileVersionInfo.GetVersionInfo(type.GetTypeInfo().Assembly.Location)?.ProductVersion ?? EmptyVersion;
+				var productVersion = "8.0.0-alpha.8+02b315d290415a4eb153beb827a879d037e904f6 (Microsoft Windows 10.0.19044; .NET 6.0.4; Elastic.Clients.Elasticsearch)"; //FileVersionInfo.GetVersionInfo(type.GetTypeInfo().Assembly.Location)?.ProductVersion ?? EmptyVersion;
 
 				if (productVersion == EmptyVersion)
 					productVersion = Assembly.GetAssembly(type).GetName().Version.ToString();
