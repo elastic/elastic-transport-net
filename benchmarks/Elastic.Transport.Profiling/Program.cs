0547ed997170110dba31143fb652553fb57e3261
Steve Gordon <sgordon@hotmail.co.uk>
2023-08-08 12:06:11 +0100

    Update profiler app

--- benchmarks/Elastic.Transport.Profiling/Program.cs
+++ benchmarks/Elastic.Transport.Profiling/Program.cs
@@ -2,7 +2,6 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-using System;
 using System.Threading.Tasks;
 using Elastic.Transport.Products.Elasticsearch;
 using JetBrains.Profiler.Api;
@@ -19,23 +18,20 @@ namespace Elastic.Transport.Profiling
 			var config = new TransportConfiguration(new Uri("http://localhost:9200"), new ElasticsearchProductRegistration());
 			var transport = new DefaultHttpTransport(config);
 
-			_ = await transport.GetAsync<VoidResponse>("/");
+			// WARMUP
+			for (var i = 0; i < 50; i++) _ = await transport.GetAsync<VoidResponse>("/");
 
-			MemoryProfiler.GetSnapshot("before-many-requests");
-
-			for (var i = 0; i < 1_000; i++) _ = await transport.GetAsync<VoidResponse>("/");
-
-			MemoryProfiler.GetSnapshot("after-many-requests");
-			//MeasureProfiler.StopCollectingData();
+			MemoryProfiler.GetSnapshot("before-100-requests");
+			for (var i = 0; i < 100; i++) _ = await transport.GetAsync<VoidResponse>("/");
+			MemoryProfiler.GetSnapshot("after-100-requests");
 
 			await Task.Delay(1000);
-
 			MemoryProfiler.ForceGc();
+
 			MemoryProfiler.GetSnapshot("before-final-request");
 			_ = await transport.GetAsync<VoidResponse>("/");
 			MemoryProfiler.GetSnapshot("after-final-request");
 
-			MemoryProfiler.ForceGc();
 			MemoryProfiler.GetSnapshot("end");
 		}
 	}
