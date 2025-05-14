01f16e509826ef0b790b35b70b1182f7fea48415
Martijn Laarman <Mpdreamz@gmail.com>
2025-05-14 19:25:38 +0200

    Expose Elasticsearch error serialization context so it can be combined with upstream contexts (#160)

---
+++ src/Elastic.Transport/Products/Elasticsearch/ErrorSerializationContext.cs
@@ -0,0 +1,14 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+
+namespace Elastic.Transport.Products.Elasticsearch;
+
+/// Adds support for serializing Elasticsearch errors using <see cref="IJsonTypeInfoResolver"/>
+[JsonSerializable(typeof(Error))]
+[JsonSerializable(typeof(ErrorCause))]
+public partial class ErrorSerializerContext : JsonSerializerContext;
